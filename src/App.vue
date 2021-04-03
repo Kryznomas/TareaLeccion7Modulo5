79e0b6e178e647e7ca9a043f226445544b783ccf
Kryznomas <kryztop@gmail.com>
2021-04-03 14:39:17 -0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,43 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <b-container>
+      <b-jumbotron
+        header="BootstrapVue"
+        lead="Bootstrap v4 Componentes para Vue.js 2"
+      >
+        <p>Más información para visitar el sitio</p>
+        <b-btn variant="primary" href="https://bootstrap-vue.org/"
+          >Más información</b-btn
+        >
+      </b-jumbotron>
+      <b-form-group
+        horizontal
+        :label-cols="4"
+        description="Ingrese su nombre."
+        label="Ingrese su nombre"
+      >
+        <b-form-input v-model.trim="name"></b-form-input>
+      </b-form-group>
+      <b-alert variant="success" :show="showAlert">Hola {{ name }}</b-alert>
+    </b-container>
   </div>
 </template>
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      name: "",
+    };
+  },
+  computed: {
+    showAlert() {
+      return this.name.length > 3 ? true : false;
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
